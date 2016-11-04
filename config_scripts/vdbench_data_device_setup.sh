f14e6e9ecdf3bfc17bd08aefd78894a22685bff8
rjrooke <rjrooke@earthlink.net>
2016-11-03 22:09:46 -0700

    put vdbench deployment playbook into file lower depth for vbench to stop panics

--- config_scripts/vdbench_data_device_setup.sh
+++ config_scripts/vdbench_data_device_setup.sh
@@ -2,7 +2,7 @@
 
 sudo cat << EOF > /home/vdbench/vdbench_data_init.vdb
 
-fsd=fsd1,anchor=/vdbench_data,depth=4,width=2,files=6,size=100M
+fsd=fsd1,anchor=/vdbench_data,depth=3,width=2,files=6,size=100M
 fwd=fwd1,fsd=fsd1,operation=read,xfersize=4k,fileio=sequential,fileselect=random,threads=2
 rd=rd1,fwd=fwd1,fwdrate=100,format=yes,elapsed=10,interval=1
 
