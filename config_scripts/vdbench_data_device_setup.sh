1570122bc8a60ff2299dea78ba6d7bb342ca4798
rjrooke <rjrooke@earthlink.net>
2016-11-03 21:41:14 -0700

    add sudo to config script

--- config_scripts/vdbench_data_device_setup.sh
+++ config_scripts/vdbench_data_device_setup.sh
@@ -1,4 +1,4 @@
-cat << EOF > /home/vdbench/vdbench_data_init.vdb
+sudo cat << EOF > /home/vdbench/vdbench_data_init.vdb
 
 fsd=fsd1,anchor=/vdbench_data,depth=4,width=2,files=6,size=100M
 fwd=fwd1,fsd=fsd1,operation=read,xfersize=4k,fileio=sequential,fileselect=random,threads=2
