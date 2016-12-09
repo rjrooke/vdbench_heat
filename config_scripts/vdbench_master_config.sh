1cb2759f030fbf608ae6f87d066151013921508f
rjrooke <rjrooke@earthlink.net>
2016-12-09 08:39:14 -0700

    create base template for vdbench test

---
+++ config_scripts/vdbench_master_config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo cat << EOF > /home/vdbench/vdbench_master_config.vdb
+
+fsd=fsd1,anchor=/vdbench_data,depth=3,width=2,files=6,size=100M
+fwd=fwd1,fsd=fsd1,operation=read,xfersize=4k,fileio=sequential,fileselect=random,threads=2
+rd=rd1,fwd=fwd1,fwdrate=100,format=yes,elapsed=10,interval=1
+
+EOF
